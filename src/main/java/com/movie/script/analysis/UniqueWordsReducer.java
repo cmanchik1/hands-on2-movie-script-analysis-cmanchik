cda1eb1151f124a938bba49d4a03cd9f1e8d1228
cmanchik1 <cmanchik@uncc.edu>
2024-09-18 04:24:13 +0000

    Completed Movie Script Analysis Assignment

--- src/main/java/com/movie/script/analysis/UniqueWordsReducer.java
+++ src/main/java/com/movie/script/analysis/UniqueWordsReducer.java
@@ -1,15 +1,20 @@
 package com.movie.script.analysis;
 
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
-
 import java.io.IOException;
 import java.util.HashSet;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 
 public class UniqueWordsReducer extends Reducer<Text, Text, Text, Text> {
 
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        HashSet<String> uniqueWords = new HashSet<>();
+        for (Text val : values) {
+            uniqueWords.add(val.toString());
+        }
+        context.write(key, new Text(String.join(", ", uniqueWords)));
 
     }
 }
